45901588581fb16942aebb113697c08871700cc6
Alex Antonison <adantonison@gmail.com>
2019-12-08 03:37:10 -0600

    Fix comment in setup bash script

--- ubuntu_scripts/2_setup_oh_my_bash.sh
+++ ubuntu_scripts/2_setup_oh_my_bash.sh
@@ -3,5 +3,6 @@
 ## Set up oh my bash
 sh -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
 
-## S
+## Add sourcing your .bashrc to
+## your .bash_profile
 echo "source ~/.bashrc" >> ~/.bash_profile
